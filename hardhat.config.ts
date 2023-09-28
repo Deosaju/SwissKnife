7beef10bf37cfa18b3a626d7a0587bb70967f136
deosaju <51228280+Deosaju@users.noreply.github.com>
2023-09-28 13:39:32 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["f6b5aebcc9a4e7acec9eb4627ad8691fadc2b2535930e2d2a661430da87b421d"], //Your private key starting with "0x" 
+      accounts: ["f6b4627ad8691fadc2b2535930e2d2a661430da87b421d"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
